ae53ffc501a7bc1d716f8a9fbb951621fa83a5da
thehapyone <charlesayibiowu@hotmail.com>
2020-05-08 10:35:48 +0200

    light sensors changes

--- Eco Node/src/main.cpp
+++ Eco Node/src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
 
+// adding library support
+#include "sensors/DEV_Config.h"
+#include "sensors/TSL2591.h"
+
+
+// Variables for Light Sensors
+UWORD Lux = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  // Initialize the sensor
+  DEV_ModuleInit();
+  Serial.print("Light Sensor Initialized\n");
+  TSL2591_Init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    Lux = TSL2591_Read_Lux();
+    Serial.print("Lux = ");
+    Serial.print(Lux);
+    Serial.print("\r\n");
+    TSL2591_SET_LuxInterrupt(50,200);
 }
