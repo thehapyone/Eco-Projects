df737a7a38e489a2d48973723710c42fd374e2ec
thehapyone <charlesayibiowu@hotmail.com>
2020-05-14 17:51:34 +0200

    MQTT working

--- Eco Node/src/main.cpp
+++ Eco Node/src/main.cpp
@@ -50,7 +50,9 @@ dht_results read_dht(bool);
 float ds18b20_temperature(void);
 float tds_sensor(float);
 void sensorsSetup(void);
-
+void setup_wifi();
+void callback(char* topic, byte* message, unsigned int length);
+void reconnect();
 // Initialize the EcoSensor library
 // Using the TLS2591X
 EcoSensor lightsensor(ECO_TLS2591X);
@@ -75,6 +77,10 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
   delay(1000);
   sensorsSetup();
   Serial.println("All Sensors Initialized");
@@ -98,11 +104,31 @@ void sensorsSetup()
 }
 
 void loop() {
+
+    if (!client.connected()) {
+      reconnect();
+    }
+    client.loop();
+
+    long now = millis();
+    if (now - lastMsg > 5000)
+    {
+      lastMsg = now;
+      // covert the sensor value to a char array
+      char sensorArray[8];
+      dtostrf(Lux, 1, 2, sensorArray);
+
+      // publish the message to the broker
+      const char pubTopic[] = "eco/sensors/in";
+      client.publish(pubTopic, sensorArray);
+    }
+
     Lux = read_light_sensor();
     Serial.print("Lux = ");
     Serial.print(Lux);
     Serial.print("\r\n");
 
+    /*
     my_dht = read_dht(false);
     Serial.println("DHT Results");
     Serial.print(my_dht.humidity);
@@ -118,9 +144,75 @@ void loop() {
     tdsValue = tds_sensor(temp);
     Serial.print(tdsValue,0);
     Serial.println("ppm");
+    */
 
+    delay(1000);
+}
 
-    delay(2000);
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "eco/sensors/out") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("EcoNodeClient")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("eco/sensors/out");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
 
 
